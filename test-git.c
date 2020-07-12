9cf020f186a01d3acc7dfd1538ff5d8113e78524
kmustbiao <1935145990@qq.com>
2020-07-12 10:36:57 +0800

    first git file

---
+++ test-git.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: test-git.c
+	> Author:ZHAO BIAO 
+	> Mail: 1935145990@qq.com
+	> Created Time: Sun 12 Jul 2020 10:34:22 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
